f2481bfa56f11400c4b6872b67cf53fec2974aa4
Nothtaro <nothnorth@outlook.com>
2021-08-18 03:16:15 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.gradle.internal.os.OperatingSystem
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    java
+}
+
+val lwjglVersion = "3.2.3"
+val jomlVersion = "1.10.1"
+
+group = "io.github.nothtaro"
+version = "0.0.1"
+
+val lwjglNatives = when (OperatingSystem.current()) {
+    OperatingSystem.LINUX   -> "natives-linux"
+    OperatingSystem.WINDOWS -> "natives-windows"
+    else -> throw Error("Unrecognized or unsupported Operating system. Please set \"lwjglNatives\" manually")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-bgfx")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nanovg")
+    implementation("org.lwjgl", "lwjgl-nuklear")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-par")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-vulkan")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
