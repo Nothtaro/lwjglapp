43fc38a1fcf01ac6b54c7da47102eb0526be7de1
Nothtaro <nothnorth@outlook.com>
2021-08-19 23:01:24 +0900

    add basic interface

--- src/main/kotlin/io/github/nothtaro/lwjglapp/LWJGLApp.kt
+++ src/main/kotlin/io/github/nothtaro/lwjglapp/LWJGLApp.kt
@@ -1,4 +1,40 @@
 package io.github.nothtaro.lwjglapp
 
-class LWJGLApp {
+class LWJGLApp(private val game : BasicGame)  {
+    fun run() {
+        this.init()
+    }
+
+    private fun init() {
+        game.init()
+    }
+}
+
+interface BasicGame {
+    fun init()
+    fun update()
+    fun render()
+    fun input()
+    fun dispose()
+}
+
+fun main() {
+    val testGame = object : BasicGame {
+        override fun init() {
+        }
+
+        override fun update() {
+        }
+
+        override fun render() {
+        }
+
+        override fun input() {
+        }
+
+        override fun dispose() {
+        }
+    }
+
+    LWJGLApp(testGame).run()
 }
